11f451d31195a20770afaf018492dbd865b8188b
salmaagina <78047530+salmaagina@users.noreply.github.com>
2021-02-16 12:40:56 -0800

    Update checksum.c

--- checksum.c
+++ checksum.c
@@ -1,37 +1,36 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
 
 
 #define max_int (255)
-#define byte (char)
+#define byte unsigned char
+
 
 int main(int argc, char * argv[], char ** envp)
 {
 
 	int count = 10;
 	int sum = 0;
-	int checksum;
-	int complement;
+	byte checksum;
+	byte complement;
 	int quotient;
 	int remainder;
-	int input;
+	byte header[10];
+
+	/* the following is the prototype for the read system call */
+	read(STDIN_FILENO, &header, count);
 
 	for (int c = 1; c <= count; c++)
 	{
-		scanf_s("%d", &input);
-		if (input > max_int)
+		if (c == 6)
 		{
-			fprintf(stderr, "Error in input \n");
-			exit(1);
+			checksum = header[c];
+			header[c] = 0;
 		}
-
-		if (c == 10)
-		{
-			checksum = input;
-			input = 0;
-		}
-
-		sum += input;
+		sum += header[c];
 	}
 
 	quotient = sum / (max_int + 1);
@@ -39,12 +38,11 @@ int main(int argc, char * argv[], char ** envp)
 	sum = quotient + remainder;
 	complement = max_int - sum;
 
-	/* the following is the prototype for the read system call */
-	int read(int fildes, void *buf, size_t nbyte);
 
 
 	fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
-	if (checksum != complement) {
+	if (checksum != complement) 
+	{
 		fprintf(stderr, "Error Detected!\n");
 		return 1;
 	}
